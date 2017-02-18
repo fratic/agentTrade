40ba83d25a8daba5db8552e590f4b6ac8c9e88b3
mirco <mircodandrea@live.it>
2017-02-18 20:47:16 +0100

    ctrl_cliente

--- agentTrade/src/agent_trade/controller/Ctrl_gestisciCliente.java
+++ agentTrade/src/agent_trade/controller/Ctrl_gestisciCliente.java
@@ -5,15 +5,10 @@ import java.util.regex.Pattern;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.sql.JoinType;
 import org.orm.PersistentException;
-import org.orm.PersistentTransaction;
 
 import agent_trade.model.M_Cliente;
-import agent_trade.persistent.AgentTradePersistentManager;
-import agent_trade.persistent.ClienteCriteria;
+
 import agent_trade.ui.PrimaryView;
 import agent_trade.ui.content.clienti.AlberoClienti;
 import agent_trade.ui.content.clienti.CercaClienteView;
@@ -21,6 +16,7 @@ import agent_trade.ui.content.clienti.DettaglioClienteView;
 import agent_trade.ui.content.clienti.Ricerca_cliente;
 import agent_trade.ui.content.clienti.RiepilogoClienteView;
 import agent_trade.ui.content.clienti.confermaCancCliente;
+import agent_trade.util.Costanti;
 
 public class Ctrl_gestisciCliente {
 
@@ -68,44 +64,44 @@ public class Ctrl_gestisciCliente {
 		String campoErrato = "";
 		
 		if(nome.equals("") || cognome.equals("") || codFiscale.equals("") || partitaIva.equals("") || citta.equals("") || cap.equals("") || indirizzo.equals("") || email.equals("") || telefono.equals("") || cellulare.equals("") || fax.equals("")){
-			return errore= "inserisci tutti i campi";
+			return errore = Costanti.MESSAGGIO_CAMPI_VUOTI;
 		}
-		if(!check("[a-zA-Z'._%-& \\s]+", cognome)){
-			campoErrato=campoErrato + "cognome ";
+		if(!check(Costanti.REG_EX_COGNOME, cognome)){
+			campoErrato = campoErrato + "cognome ";
 		}
-		if(!check("[a-zA-Z'\\s]+", nome)){
-			campoErrato= campoErrato + "nome ";
+		if(!check(Costanti.REG_EX_NOME, nome)){
+			campoErrato = campoErrato + "nome ";
 		}
-		if(!check("[a-zA-Z]{6}\\d\\d[a-zA-Z]\\d\\d[a-zA-Z]\\d\\d\\d[a-zA-Z]", codFiscale)){
-			campoErrato=campoErrato + "codice fiscale ";
+		if(!check(Costanti.REG_EX_COD_FISC, codFiscale)){
+			campoErrato = campoErrato + "codice fiscale ";
 		}
-		if(!check("[0-9]{11}", partitaIva)){
-			campoErrato=campoErrato + "partita iva ";
+		if(!check(Costanti.REG_EX_PART_IVA, partitaIva)){
+			campoErrato = campoErrato + "partita iva ";
 		}
-		if(!check("[a-zA-Z'. \\s]+[0-9]{0,4}", indirizzo)){
-			campoErrato= campoErrato +"indirizzo ";
+		if(!check(Costanti.REG_EX_INDIRIZZO, indirizzo)){
+			campoErrato = campoErrato +"indirizzo ";
 		}
-		if(!check("[a-zA-Z'\\s]+", citta)){
-			campoErrato= campoErrato +"città ";
+		if(!check(Costanti.REG_EX_CITTA, citta)){
+			campoErrato = campoErrato +"città ";
 		}
-		if(!check("[0-9]{5}", cap)){
-			campoErrato= campoErrato +"cap ";
+		if(!check(Costanti.REG_EX_CAP, cap)){
+			campoErrato = campoErrato +"cap ";
 		}
-		if(!check("[0-9]{10}", telefono)){
-			campoErrato= campoErrato +"telefono ";
+		if(!check(Costanti.REG_EX_TELEFONO, telefono)){
+			campoErrato = campoErrato +"telefono ";
 		}
-		if(!check("[0-9]{10}", cellulare)){
-			campoErrato= campoErrato +"cellulare ";
+		if(!check(Costanti.REG_EX_CELLULARE, cellulare)){
+			campoErrato = campoErrato +"cellulare ";
 		}
-		if(!check("[0-9]{10}", fax)){
-			campoErrato= campoErrato +"fax ";
+		if(!check(Costanti.REG_EX_FAX, fax)){
+			campoErrato = campoErrato +"fax ";
 		}
-		if(!check("[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}", email)){
-			campoErrato= campoErrato +"email ";
+		if(!check(Costanti.REG_EX_EMAIL, email)){
+			campoErrato = campoErrato +"email ";
 		}
 		if (errore == null && campoErrato != "")
 		{
-			errore= "I seguenti campi sono errati: "+ campoErrato;
+			errore = Costanti.MESSAGGIO_CAMPI_ERRATI + campoErrato;
 		}
 		return errore;
 	}
@@ -121,31 +117,14 @@ public class Ctrl_gestisciCliente {
 		}
 		else
 		{
-			M_Cliente [] listClienti=null;
-
-			try{
-				
-				ClienteCriteria criteriaCliente= new ClienteCriteria();
-				
-				//JOIN per recuperare solo i clienti dell'agente loggato
-				criteriaCliente.createCriteria("agenteAssociato", "IdAgente", JoinType.INNER_JOIN,   Restrictions.eq("IdAgente", Ctrl_System.getAgenteLog().getIdAgente())); 
-			
-				//BISOGNA RIPORTARE LA STRINGA TUTTA IN MINUSCOLO PERCHE è CASE SENSITIVE				
-				criteriaCliente.cognome.like("%"+c+"%");
-				
-				listClienti = criteriaCliente.listCliente();
-
-			}
-			finally {
-				AgentTradePersistentManager.instance().disposePersistentManager();
-			}
+			M_Cliente [] listClienti = M_Cliente.caricaClientiCognome(c);
 						
 			if (listClienti.length!=0)
 			{
 				CercaClienteView.getInstance().popolaTab(listClienti);
 			}
 			else{		
-				CercaClienteView.getInstance().setErrore("Cliente non trovato");
+				CercaClienteView.getInstance().setErrore(Costanti.MESSAGGIO_CLIENTE_NON_TROVATO);
 			}
 		}
 	}
@@ -159,31 +138,12 @@ public class Ctrl_gestisciCliente {
 		}
 		else {
 			
-			M_Cliente [] listClienti=null;
-			
-			try{
-				
-				ClienteCriteria criteriaCliente= new ClienteCriteria();
-				
-				//JOIN per recuperare solo i clienti dell'agente loggato
-				criteriaCliente.createCriteria("agenteAssociato", "IdAgente", JoinType.INNER_JOIN,   Restrictions.eq("IdAgente", Ctrl_System.getAgenteLog().getIdAgente())); 
-				
-				//BISOGNA RIPORTARE LA STRINGA TUTTA IN MINUSCOLO PERCHE è CASE SENSITIVE				
-				criteriaCliente.cognome.like("%"+c+"%");
-				criteriaCliente.partita_iva.like("%"+pi+"%");
-				criteriaCliente.codice_fiscale.like("%"+cf+"%");
-				criteriaCliente.citta.like("%"+city+"%");
-			
-				listClienti = criteriaCliente.listCliente();
-
-			}
-			finally {
-				AgentTradePersistentManager.instance().disposePersistentManager();
-			}
+			M_Cliente [] listClienti = M_Cliente.caricaClientiParametri(c, pi, cf, city);
 			
 			if(listClienti.length==0) {
 		
-				Ricerca_cliente.getInstance().setErrore("Cliente non trovato");
+				Ricerca_cliente.getInstance().setErrore(Costanti.MESSAGGIO_CLIENTE_NON_TROVATO);
+				Ricerca_cliente.getInstance().setVisibleErroreRicercaCliente(true);
 			}
 		
 			else{
@@ -210,11 +170,7 @@ public class Ctrl_gestisciCliente {
 	
 			PrimaryView.getInstance().resetPannelloCentraleCliente();
 			
-			ClienteCriteria criteria= new ClienteCriteria();
-			criteria.idCliente.eq(idCliente);
-			criteria.agenteAssociato.equals(Ctrl_System.getInstance());
-
-			M_Cliente cliente = criteria.listCliente()[0];
+			M_Cliente cliente = M_Cliente.cercaCliente(idCliente);
 			
 			Ricerca_cliente.getInstance().dispose();
 			Ricerca_cliente.cancInstanza();
@@ -237,56 +193,37 @@ public class Ctrl_gestisciCliente {
 		String errore= ControlloCampi(nome, cognome, codFiscale, partitaIva, citta, cap, indirizzo, email, telefono, cellulare, fax);
 		if(errore==null)
 		{
-			PersistentTransaction t = AgentTradePersistentManager.instance().getSession().beginTransaction();
-			try 
-			{				
-				M_Cliente cliente=new M_Cliente();
+			M_Cliente cliente = new M_Cliente();
 				
-				cliente.setNome(nome);
-				cliente.setCognome(cognome);
-				cliente.setCodice_fiscale(codFiscale);
-				cliente.setPartita_iva(partitaIva);
-				cliente.setIndirizzo(indirizzo);
-				cliente.setEmail(email);
-				cliente.setTelefono(telefono);
-				cliente.setFax(fax);
-				cliente.setCitta(citta);
-				cliente.setCAP(cap);
-				cliente.setCell(cellulare);
-				
-				AgentTradePersistentManager.instance().getSession().save(cliente);
-				
-				// commit per il salvataggio
-				t.commit();
+			cliente.setNome(nome);
+			cliente.setCognome(cognome);
+			cliente.setCodice_fiscale(codFiscale);
+			cliente.setPartita_iva(partitaIva);
+			cliente.setIndirizzo(indirizzo);
+			cliente.setEmail(email);
+			cliente.setTelefono(telefono);
+			cliente.setFax(fax);
+			cliente.setCitta(citta);
+			cliente.setCAP(cap);
+			cliente.setCell(cellulare);
+			cliente.setAttivo(1);
+			
+			M_Cliente.salvaCliente(cliente);
 
-				PrimaryView.getInstance().resetNuovoCliente();
-				PrimaryView.getInstance().resetPannelloCentraleCliente();
+			PrimaryView.getInstance().resetNuovoCliente();
+			PrimaryView.getInstance().resetPannelloCentraleCliente();
 				
-				//Che fa?? secondo me ha un alto accoppiamento e una bassa coesione
-//				M: SERVE PER VISUALIZZARE IL CLIENTE APPENA INSERITO MA COME LO RECUPERO SE NON CONOSCO ANCORA L'ID?
+			int id = M_Cliente.getMaxId();
+			recuperaCliente(id);
 				
-//				M: MODIFICHE PER RECUPERARE ULTIMO ID (prossime 3 righe)
-				ClienteCriteria criteria= new ClienteCriteria();
-				criteria.setProjection(Projections.max("id"));
-				int id=(int) criteria.uniqueResult();
-				
-				recuperaCliente(id);
-				
-				AlberoClienti.inserisciNodo(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
-				AlberoClienti.selectNode(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
-				AlberoClienti.abilitaAlbero();
+			AlberoClienti.inserisciNodo(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
+			AlberoClienti.selectNode(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
+			AlberoClienti.abilitaAlbero();
 			
-				PrimaryView.getInstance().setEnableTabPreventivo(true);
-				PrimaryView.getInstance().setEnableTabCatalogo(true);
-				PrimaryView.getInstance().setEnableCercaCliente(true);
-			}
+			PrimaryView.getInstance().setEnableTabPreventivo(true);
+			PrimaryView.getInstance().setEnableTabCatalogo(true);
+			PrimaryView.getInstance().setEnableCercaCliente(true);
 			
-			catch (Exception e) {
-				t.rollback();
-			}
-			finally {
-				AgentTradePersistentManager.instance().disposePersistentManager();
-			}
 		}
 		else{
 			PrimaryView.getInstance().setVisibleErroreNuovoCliente(true);
@@ -297,25 +234,11 @@ public class Ctrl_gestisciCliente {
 	
 	public void modificaCliente(int id, String nome, String cognome, String codFiscale, String partitaIva, String citta, String cap, String indirizzo, String email, String telefono, String cellulare, String fax) throws PersistentException
 	{
-		String errore= ControlloCampi(nome, cognome, codFiscale, partitaIva, citta, cap, indirizzo, email, telefono, cellulare, fax);
+		String errore = ControlloCampi(nome, cognome, codFiscale, partitaIva, citta, cap, indirizzo, email, telefono, cellulare, fax);
 		
-		if(errore==null)
+		if(errore == null)
 		{	
-			M_Cliente cliente = null;	
-			try{
-				
-				ClienteCriteria criteriaCliente= new ClienteCriteria();
-				
-				//JOIN per recuperare solo i clienti dell'agente loggato
-				criteriaCliente.createCriteria("agenteAssociato", "IdAgente", JoinType.INNER_JOIN,   Restrictions.eq("IdAgente", Ctrl_System.getAgenteLog().getIdAgente())); 
-				criteriaCliente.idCliente.eq(id);
-				
-				cliente = criteriaCliente.listCliente()[0];
-
-			}
-			finally {
-				AgentTradePersistentManager.instance().disposePersistentManager();
-			}
+			M_Cliente cliente = M_Cliente.cercaCliente(id);
 		
 			cliente.setNome(nome);
 			cliente.setCognome(cognome);
@@ -330,14 +253,7 @@ public class Ctrl_gestisciCliente {
 			cliente.setCAP(cap);
 			cliente.setCell(cellulare);
 			
-			PersistentTransaction t = AgentTradePersistentManager.instance().getSession().beginTransaction();
-			try {
-				AgentTradePersistentManager.instance().getSession().update(cliente);
-				t.commit();
-			}
-			catch (Exception e) {
-				t.rollback();
-			}
+			M_Cliente.aggiornaCliente(cliente);
 							
 			PrimaryView.getInstance().resetCliente();
 			PrimaryView.getInstance().disattivaModifica(true);
@@ -353,9 +269,7 @@ public class Ctrl_gestisciCliente {
 			AlberoClienti.updateNodo(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
 			AlberoClienti.selectNode(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
 			AlberoClienti.abilitaAlbero();
-			
-			
-						
+								
 		}
 		else{
 			PrimaryView.getInstance().setVisibleErroreRiepCliente(true);
@@ -373,24 +287,7 @@ public class Ctrl_gestisciCliente {
 	//bisogna decidere il criterio di caricamento. Decidere se è adeguata questa struttura dati
 	public M_Cliente [] caricaClienti() throws PersistentException 
 	{
-		M_Cliente [] listClienti=null;
-		try{
-			
-			ClienteCriteria criteriaCliente= new ClienteCriteria();
-			
-			//JOIN per recuperare solo i clienti dell'agente loggato
-			criteriaCliente.createCriteria("agenteAssociato", "IdAgente", JoinType.INNER_JOIN,   Restrictions.eq("IdAgente", Ctrl_System.getAgenteLog().getIdAgente())); 
-			listClienti = criteriaCliente.listCliente();
-//
-//			for (M_Cliente cLoad : listClienti) {
-//				//aggiustare, crea doppioni nell albero
-//				AlberoClienti.inserisciNodo(cLoad.getCognome()+ " - " +cLoad.getNome());
-//			}
-		}
-		finally {
-			AgentTradePersistentManager.instance().disposePersistentManager();
-		}
-		
+		M_Cliente [] listClienti = M_Cliente.caricaClientiAgente();
 		return listClienti;
 	}
 
@@ -483,30 +380,10 @@ public class Ctrl_gestisciCliente {
 		
 		int idCliente=Integer.parseInt(id);
 		
-		M_Cliente cliente=null;
-		PersistentTransaction t = AgentTradePersistentManager.instance().getSession().beginTransaction();
+		M_Cliente cliente=M_Cliente.cercaCliente(idCliente);
+		cliente.setAttivo(0);
+		M_Cliente.aggiornaCliente(cliente);
 
-		try{
-			
-			ClienteCriteria criteriaCliente= new ClienteCriteria();
-			
-			//JOIN per recuperare solo i clienti dell'agente loggato
-			criteriaCliente.createCriteria("agenteAssociato", "IdAgente", JoinType.INNER_JOIN,   Restrictions.eq("IdAgente", Ctrl_System.getAgenteLog().getIdAgente())); 
-			criteriaCliente.idCliente.eq(idCliente);
-			cliente = criteriaCliente.listCliente()[0];
-
-			AgentTradePersistentManager.instance().getSession().delete(cliente);
-			
-			t.commit();
-		}
-
-		catch (Exception e) {
-			t.rollback();
-		}	
-		finally {
-			AgentTradePersistentManager.instance().disposePersistentManager();
-		}
-//		M: PROBLEMA CON LA SELEZIONE DELL'ALBERO
 		AlberoClienti.rimuoviNodo(cliente.getIdCliente()+ " - " +cliente.getCognome()+ " - " +cliente.getNome());
 		confermaCancCliente.getInstance().setVisible(false);		
 		confermaCancCliente.cancInst();
