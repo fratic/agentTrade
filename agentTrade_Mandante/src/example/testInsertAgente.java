0cea954fd006892cf5cd3a7ae2cfecd30021a22b
fratic <fraticelli.antonio@gmail.com>
2017-03-07 13:18:25 +0100

    db remoto

--- agentTrade_Mandante/src/example/testInsertAgente.java
+++ agentTrade_Mandante/src/example/testInsertAgente.java
@@ -8,7 +8,7 @@ public class testInsertAgente {
 
 	public static void main(String[] args) throws PersistentException {
 
-		Ctrl_gestisciAgente.getInstance().nuovoAgente("Antonio", "Fraticelli", "antofrat", "asd", "sgr", "71013", "via foggia 217", "fratic@asd.it", "3204578987", 3);
+		Ctrl_gestisciAgente.getInstance().nuovoAgente("mario", "Fraticelli", "antofrat", "asd", "sgr", "71013", "via foggia 217", "fratic@asd.it", "3204578987", 3);
 		
 	}
 
